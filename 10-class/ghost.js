c526d1d69a4901f81a23ea463a6d9403c74611a8
viktoriya1234 <vikapav007@gmail.com>
2025-05-31 14:08:46 +0300

    ls11

--- 10-class/ghost.js
+++ 10-class/ghost.js
@@ -8,9 +8,12 @@ const cHeight = canvas.height;
 const imgFolder = 'images/'
 
 const bgImg = new Image()
-bgImg.src = imgFolder + 'map01_preview-01.png';
+bgImg.src = imgFolder + 'map-01.png';
 
 let gameFrame = 0
+let isRunning = true;
+let reqestId = null;
+let score = 0;
 
 let mouse = {
     x: cWidth / 2,
@@ -44,11 +47,11 @@ class Player {
         this.sWidth = 43
         this.sHeight = 50
         this.takt = 7
-        this.speed = 1
+        this.speed = 3
 
     }
     drawImg(img, maxFrame){
-         console.log(mouse, this.xFrame)
+         //console.log(mouse, this.xFrame)
         ctx.drawImage(
             img,
             this.sWidth * this.xFrame,
@@ -67,15 +70,26 @@ class Player {
 
 
     stay() {
-        this.drawImg(player.heroImg, this.heroMaxFrame)
+        this.drawImg(Player.heroImg, this.heroMaxFrame)
     }
     runLeft() {
-        this.drawImg(player.runLeftImg, this.heroMaxFrame)
+        this.drawImg(Player.runLeftImg, this.runMaxFrame)
     }
     runRight() {
-        this.drawImg(player.runRightImg, this.heroMaxFrame)
+        this.drawImg(Player.runRightImg, this.runMaxFrame)
     }
     update(){
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy)
+
+        if(distance > this.speed){
+            this.x += (dx / distance) * this.speed
+            this.y += (dy / distance) * this.speed
+        } else {
+            this.x = mouse.x;
+            this.y = mouse.y
+        }
 
     }
     move(){
@@ -90,13 +104,79 @@ class Player {
 }
 
 class Ghost{
+    static idleGhost = new Image();
 
+    constructor(){
+        this.runMaxFrame = 11
+        this.xFrame = 0
+        this.sWidth = 43
+        this.sHeight = 50
+        this.takt = 7
+
+        this.x = cWidth + 43 ;
+        this.y =  Math.random() * (cHeight - this.sHeight);
+        this.speed = Math.random() * 5
+
+    }
+    move(){
+        console.log(this.x, this.y)
+        this.x -= this.speed;
+
+         ctx.drawImage(
+            Ghost.idleGhost,
+            this.sWidth * this.xFrame,
+            0,
+            this.sWidth,
+            this.sHeight,
+            this.x,
+            this.y,
+            this.sWidth,
+            this.sHeight 
+        )
+        if (gameFrame % this.takt === 0){
+            this.xFrame = (this.xFrame + 1) % this.runMaxFrame
+        }
+    }
+}
+
+Ghost.idleGhost.src = imgFolder + 'ghostLeft.png'
+
+function checkCollision(obj1, obj2){
+    let dx = obj1.x - obj2.x
+    let dy = obj1.y - obj2.y
+
+    let distance = Math.sqrt(dx*dx + dy*dy)
+    return distance < obj1.sHeight / 2 + obj2.sHeight / 2;
+}
+
+let ghostArray = [];
+
+function ghostMaker() {
+    if (gameFrame % 50 === 0 ){
+        //console.log(ghostArray)
+        ghostArray.push(new Ghost())
+    }
+
+    ghostArray.forEach(ghost => ghost.move())
+
+    ghostArray = ghostArray.filter(ghost => {
+        if (ghost.x < - ghost.sWidth) return false;
+
+        if (checkCollision(player, ghost)){
+            score++
+            return false
+        }
+    })
 }
 
 let player = new Player(cWidth / 2, cHeight / 2)
-let ghost = new Ghost()
+Player.loadImages()
+
+//console.log(bgImg);
 
 function start() {
+    if (!isRunning) return;
+
     //console.log(gameFrame);
     ctx.clearRect(0,0, cWidth, cHeight)
     ctx.drawImage(bgImg, 0, 0, cWidth, cHeight)
@@ -104,11 +184,33 @@ function start() {
     //cod . . .
     player.update()
     player.move()
+    ghostMaker()
+
 
     gameFrame++;
-    requestAnimationFrame(start)
+    reqestId = requestAnimationFrame(start)
 }
 
 
+document.addEventListener('keydown', (e)=>{
+    //console.log(e)
+    if(e.key === 'Escape'){
+        isRunning = !isRunning;
+        if(isRunning){
+            start()
+        } else {
+            cancelAnimationFrame(reqestId)
+        }
+    }
+});
+
+const images = [bgImg, Player.heroImg,Player.runLeftImg,Player.runRightImg]
 //setInterval(start, 100)
-start()
+let loadedImages = 0
+images.forEach(img => {
+    img.onload = () => {
+        loadedImages++
+        if (loadedImages === images.length)
+            start()
+    }
+})
