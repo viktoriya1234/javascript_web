1f569396e75c2d9e7faad442faf41809fe431c81
viktoriya1234 <vikapav007@gmail.com>
2025-06-28 14:13:36 +0300

    ls16

--- 13-15-photo-keeper/static/js/pk.js
+++ 13-15-photo-keeper/static/js/pk.js
@@ -8,9 +8,93 @@ class MainMenu {
     }
     Init(){
         this.trigger.addEventListener('click', (e) => {
-            this.container.classList.toggle('menuOpened')
+            this.container.classList.toggle('mainMenuOpened')
         })
     }
 }
 
-new MainMenu('.menuTrigger', '.menuContainer')
+class PhotoGallery{
+    constructor (){
+        this.allComments = [
+        'Цей кадр нереально крутий! :)',
+        'Ти вмієш дивувати! Кожен кадр - поєднання життєлюбності і краси',
+        'Спинися мить, прекрасна ти!',
+        'Просто супер! Як тобі це вдається?',
+        'Це просто шедевр мистецтва',
+        'В цьому штучному світі так приємно знайти щось натуральне))',
+        'Клас!!!))',
+        'Нереально чудово!',
+        'А ти вмієш дивувати ;)',
+        'Це фото так і проситься в рамочку на стіну'
+        ];
+        this.allDescriptions = [
+            'Коли радості немає меж',
+            'Любов в кожному пікселі',
+            'Фото заряджене позитивом',
+            'Зловив дзен',
+            'Як мало потрібно для щастя',
+            'Знали б ви що в мене на умі! ;)',
+            'Show must go on',
+            'Good vibes only',
+            'My inspiration',
+            'On my way to paradise',
+            'Що це, якщо не любов? Х)'
+        ];
+
+        this.picturesDB = this.generatePicturesDB(25)
+        console.log(this.picturesDB)
+        this.pictureContainer = document.querySelector('.picturesContainer')
+    }
+    randomElement(array){
+        return array[Math.floor(Math.random() * array.length)]
+    }
+    generatePicturesDB(number){
+        const pictures = []
+
+        for (let i = 0; i < number; i++) {
+            const comments = [];
+            const commentCount = Math.random(Math.random() * 5)
+            
+            for (let j = 0; j < commentCount; j++)
+                comments.push(this.randomElement(this.allComments))
+
+            
+            pictures.push({
+                src: `../static/img/photos/${i}.jpg`,
+                likes: Math.floor(Math.random() * 100),
+                effect: 'none',
+                description: this.randomElement(this.allDescriptions),
+                comments: ['коментар1', 'коментар2'],
+                commentsNumber: comments.length
+                });
+            }
+
+            return pictures;
+
+    }
+    showPictures() {
+        const pictureTemplate = document.getElementById('templatePictureExample')
+        const pictureExample = pictureTemplate.content.querySelector('.pictureExample')
+        this.pictureContainer.innerHtml = '';
+
+        this.picturesDB.forEach( (photo) => {
+            const photoBlock = pictureExample.cloneNode(true)
+            photoBlock.querySelector('.pictureImg').src = photo.src;
+            photoBlock.querySelector('.pictureImg').style.filter = photo.effect
+            photoBlock.querySelector('.pictureComments').innerText = photo.commentsNumber
+            photoBlock.querySelector('.pictureStars').innerText = photo.likes
+
+            this.pictureContainer.append(photoBlock)
+        })
+
+        //console.log(pictureExample)//
+    }
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    new MainMenu('.menuTrigger', '.mainMenuContainer')
+    const gallery = new PhotoGallery();
+    gallery.showPictures();
+
+
+})
